a1b02de5dac346ea7dfc17177e8a00d92c213e9f
JTheologidis <99328219+JTheologidis@users.noreply.github.com>
2022-02-16 09:51:11 +1300

    Add files via upload
    Help GUI for Temperature Converter
    - Added Background Color (red)
    - Added dialogue box, reads 'Temperature Converter'

--- 00_Help_GUI.py
+++ 00_Help_GUI.py
@@ -1,10 +1,25 @@
 from tkinter import *
+from functools import partial    # To prevent unwanted windows
 import random
+from tkinter import font
 
 
 class Converter: 
     def __init__(self, parent):
         print("hello world")
+
+        # Formatting variables...
+        background_color = "red"
+
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame.grid()
+
+        # Temperature Conversion Heading (rov 0)
+        self.temp_converter_label = Label(text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
 
 
 # main routine
