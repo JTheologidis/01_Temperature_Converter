4da66780bec5792eba09b3b661df60b56daa75e8
JTheologidis <99328219+JTheologidis@users.noreply.github.com>
2022-02-17 14:39:55 +1300

    Add files via upload
    Help GUI for Temperature Converter
    - Added Help Button

--- 00_Help_GUI.py
+++ 00_Help_GUI.py
@@ -1,8 +1,7 @@
 from tkinter import *
 from functools import partial    # To prevent unwanted windows
-import random
-from tkinter import font
 
+import random
 
 class Converter: 
     def __init__(self, parent):
@@ -12,15 +11,27 @@ class Converter:
         background_color = "red"
 
         # Converter Main Screen GUI...
-        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame = Frame(width=600, height=600, bg=background_color,
+                                     pady=10)
         self.converter_frame.grid()
 
         # Temperature Conversion Heading (rov 0)
-        self.temp_converter_label = Label(text="Temperature Converter",
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
                                           font=("Georgia", "18", "bold"),
+                                          bg=background_color,
                                           padx=10, pady=10)
         self.temp_converter_label.grid(row=0)
 
+        # Help Button (row 1)
+        self.help_button = Button (self.converter_frame, text="help", 
+                                   font= ("Arial", "14"),
+                                   padx=10, pady=10, command=self.help)
+        self.help_button.grid(row=1)
+        
+    def help(self):
+        print("you asked for help")
+        get_help = Help(self)
+        get_help.help_text.configure(text="Help text goes here")
 
 # main routine
 if __name__ == "__main__":
