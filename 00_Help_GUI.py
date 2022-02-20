879fcbc6c5e13e33c7e1dea6f9fb42f81db59f15
JTheologidis <99328219+JTheologidis@users.noreply.github.com>
2022-02-21 12:06:17 +1300

    Partially Working GUI with help window (fixed)
    - fixed help button, reappears when users press cross at the top of help window

--- 00_Help_GUI.py
+++ 00_Help_GUI.py
@@ -8,7 +8,7 @@ class Converter:
         print("hello world")
 
         # Formatting variables...
-        background_color = "purple"
+        background_color = "#d19fe8"    #light purple
 
         # Converter Main Screen GUI...
         self.converter_frame = Frame(width=600, height=600, bg=background_color,
@@ -44,6 +44,9 @@ class Help:
         
         # Sets up child window (ie: help box)
         self.help_box = Toplevel()
+
+        # If users press cross at the top, closes help and 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
 
         # Set up GUI Frame
         self.help_frame = Frame(self.help_box, width=300, bg=background)
