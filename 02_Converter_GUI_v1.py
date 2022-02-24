b2e7aa40c867566a7f1e82f206f37b4be0a0a3d2
JTheologidis <99328219+JTheologidis@users.noreply.github.com>
2022-02-24 14:53:37 +1300

    Partially Working Converter GUI
    - Added "To Centigrade" and "To Fahrenheit" buttons
    - Added functional dialogue input box

--- 02_Converter_GUI_v1.py
+++ 02_Converter_GUI_v1.py
@@ -32,8 +32,23 @@ class Converter:
         self.temp_instructions_label.grid(row=1)
 
         # Temperature entry box (row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=18,
+                                      font="Arial 14 bold")
+        self.to_convert_entry.grid(row=2)
 
-        # Conversion buttons frame (row 3)
+        # Conversion buttons frame (row 3), indianred1 | springgreen2
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
+
+        self.to_c_button = Button(self.conversion_buttons_frame,
+                                  text="To Centrigrade", font="Arial 10 bold",
+                                  bg="Indianred1", padx=10, pady=10)
+        self.to_c_button.grid(row=0, column=0)
+
+        self.to_f_button = Button(self.conversion_buttons_frame,
+                                  text="To Fahrenheit", font="Arial 10 bold",
+                                  bg="springgreen2", padx=10, pady=10)
+        self.to_f_button.grid(row=0, column=1)
         
         # Answer label (row 4)
 
